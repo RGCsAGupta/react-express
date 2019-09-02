91ab9633f6cdbf76f226ca78e409a5ac5f143c00
Akash Gupta <agupta@i-sight.com>
2019-09-02 14:29:02 -0400

    basic scaffold

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Contact from "./components/Contact";
+import Grid from "./components/Grid";
+import List from "./components/List";
+import Master_Detail from "./components/Master_Detail";
+import Home from "./components/Home";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Redirect exact path = "/" to = "/Home" />
+          <Route path = "/Home" component = { Home } />
+          <Route path = "/Contact" component = { Contact } />
+          <Route path = "/Grid" component = { Grid } />
+          <Route path = "/List" component = { List } />
+          <Route path = "/Master_Detail" component = { Master_Detail } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,38 @@
+﻿import React from "react";
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className="container-fluid">
+        <div className="row justify-content-around">
+          <div className="col-8 col-md-5">
+            <h5 className={styles.title}>A.A. Info Corp.</h5>
+            <p className={styles.description}>
+              Let's build your business together.
+            </p>
+          </div>
+          <div className="col-2">
+            <ul className="list-unstyled">
+              <li>
+                <a className={styles.footerlink} href="/Home">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className={styles.footerlink} href="/Contact">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a className={styles.footerlink} href="/About_Us">
+                  About Us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
